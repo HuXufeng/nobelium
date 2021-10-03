cc3b8e0092c268654a27d8a0e822fe2f904b2b72
胡旭峰 <huxufeng0120@gmail.com>
2021-10-03 20:27:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://nobelium-phi-gold.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://weibo.com/wushiohfon',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
